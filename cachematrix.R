f1e5ea7fa7be5beef92a7e2c88a714bc61c9028b
Saurav Mitra <saurav.karate@gmail.com>
2014-04-22 00:20:08 +0800

    third commit

--- cachematrix.R
+++ cachematrix.R
@@ -28,11 +28,34 @@ cacheSolve <- function ( x, ... ) {
     m <- x$getInvMat( )
     if ( !is.null ( m ) ) {
     ## Getting Inverse of a Matrix from Cached Data
+        # print ( "Getting Inverse of a Matrix from Cached Data" )
         return ( m )
     }
     data <- x$get( )
     ## Calculating Inverse of a Matrix for first time
     m <- solve ( data, ... )
     x$setInvMat( m )
-    m
+    # print ( "Calculating Inverse of a Matrix for first time" )
+    return( m )
 }
+
+############# Sample Execution Data Set ##################
+
+## source('C:/Code/ProgrammingAssignment2/cachematrix.R')
+## mat1 <- matrix ( c ( 1, 0, 0, 1 ), 2, 2 )
+## x <- makeCacheMatrix ( mat1 )
+## cacheSolve ( x )
+
+## mat2 <- matrix ( c ( 4, 3, 3, 2 ), 2, 2 )
+## y <- makeCacheMatrix ( mat2 )
+## cacheSolve ( y )
+
+## mat3 <- matrix ( c ( 1, 2, 3, 4 ), 2, 2 )
+## z <- makeCacheMatrix ( mat3 )
+## cacheSolve ( z )
+
+## cacheSolve ( y )
+## cacheSolve ( x )
+## cacheSolve ( z )
+
+##########################################################
